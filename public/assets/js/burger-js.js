96b7f518228394253fd1362b7de990e67db1935a
Lia <liaswapna@gmail.com>
2019-02-12 16:40:38 -0800

    updated display

--- public/assets/js/burger-js.js
+++ public/assets/js/burger-js.js
@@ -21,6 +21,7 @@ $(function () {
         e.preventDefault()
         var input = "#"+ $(this).data("input");
         console.log(input)
+        console.log(document.getElementById(`${input}`).value)
         // var name = $(`${input}`).val().trim()
         console.log($("#customerName"))
         console.log($('input:textbox').val())
